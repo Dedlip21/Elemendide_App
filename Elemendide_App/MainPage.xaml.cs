014a33f85d387bb9cbde58bf43cbfef1d3c782f8
Opilane TTHK <opilane@tthk.ee>
2022-02-03 15:18:02 +0200

    Add project files.

---
+++ Elemendide_App/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Elemendide_App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            StackLayout st = new StackLayout();
+            Button Ent_btn = new Button
+            {
+                Text = "Entry",
+                BackgroundColor = Color.Fuchsia
+            };
+            st.Children.Add(Ent_btn);
+            st.BackgroundColor = Color.Aqua;
+            Content = st;
+
+            Button Timer_btn = new Button
+            {
+                Text = "Entry",
+                BackgroundColor = Color.Fuchsia
+            }; 
+
+
+            Ent_btn.Clicked += Ent_btn_Clicked;
+        }
+
+        private async void Ent_btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Entry_Page());
+        }
+    }
+}
---
+++ Elemendide_App/Timer_Page.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Elemendide_App
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Timer_Page : ContentPage
+    {
+        public Timer_Page()
+        {
+            InitializeComponent();
+        }
+
+        private void timer_btn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
